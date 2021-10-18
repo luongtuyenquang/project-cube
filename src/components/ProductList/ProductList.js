211c316a54ddd808f09dcaa47721bbd135bf113a
TuyenQuang <luongtuyenquang77@gmail.com>
2021-10-18 14:55:23 +0700

    C6 - CRUD-Products -  Get Data by Axios

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,12 +1,27 @@
 import './ProductList.css';
 import ProductItem from "../ProductItem/ProductItem";
 import { connect } from "react-redux"
+import { useEffect, useState } from 'react';
+const axios = require('axios');
 
- function ProductList(props) {
-    const renderProduct = props.products.map((product, index) => {
+
+function ProductList(props) {
+    const [products, setProduct] = useState([])
+    
+     useEffect(() => {
+        async function fetchAPI(){
+            const url = 'https://616d1e8637f997001745d866.mockapi.io/api/products'
+            const res = await axios.get(url)
+            setProduct(res.data)
+        }
+        fetchAPI()
+    }, [])
+    
+    const renderProduct = products.map((product, index) => {
         return <ProductItem product={product} key={index}/>
     })
-    
+
+
     return (
         <div className="panel panel-success">
             <div className="panel-heading heading-color">Danh sách sản phẩm</div>
